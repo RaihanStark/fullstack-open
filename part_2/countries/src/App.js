493a1870f47baaa8c4cb7daf23adcd99cc468ec4
RaihanStark <raihanyudosaputra30@gmail.com>
2021-12-30 06:45:06 +0700

    finish 2.14 or part_2b

--- part_2/countries/src/App.js
+++ part_2/countries/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+
 import axios from "axios";
 
 import CountryInfo from "./components/CountryInfo";
@@ -8,11 +9,20 @@ function App() {
   const [countryName, setCountryName] = useState("");
   const [filteredCountries, setFilteredCountries] = useState([]);
 
+  const [countryInfo, setCountryInfo] = useState([]);
+  const [showInfo, setShowInfo] = useState(false);
+
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
       setCountries(response.data);
     });
   }, []);
+
+  useEffect(() => {
+    if (filteredCountries.length === 1) {
+      setShowInfo(false);
+    }
+  }, [filteredCountries]);
 
   const filterCountries = (e) => {
     const filteredCountriesObj = countries.filter((country) =>
@@ -21,6 +31,11 @@ function App() {
 
     setCountryName(e.target.value);
     setFilteredCountries(filteredCountriesObj);
+  };
+
+  const showCountryInfo = (countryCca2) => {
+    setCountryInfo(countries.filter((country) => country.cca2 === countryCca2));
+    setShowInfo(true);
   };
 
   return (
@@ -34,13 +49,22 @@ function App() {
         </div>
       )}
 
+      {showInfo && <CountryInfo country={countryInfo[0]} />}
+
       {filteredCountries.length > 10 ? (
         "Too many matches, specify another filter"
       ) : filteredCountries.length === 1 ? (
         <CountryInfo country={filteredCountries[0]} />
       ) : (
         filteredCountries.map((country) => {
-          return <div key={country.name.common}>{country.name.common}</div>;
+          return (
+            <div key={country.name.common}>
+              {country.name.common}{" "}
+              <button onClick={() => showCountryInfo(country.cca2)}>
+                Show
+              </button>
+            </div>
+          );
         })
       )}
     </>
--- part_2/countries/src/components/CountryInfo.jsx
+++ part_2/countries/src/components/CountryInfo.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
 
 function CountryInfo({ country }) {
+  const [capitalWeather, setCapitalWeather] = useState();
+  const [weatherLoading, setWeatherLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&appid=${process.env.REACT_APP_API_KEY}&units=metric`
+      )
+      .then((response) => {
+        setCapitalWeather(response.data);
+        setWeatherLoading(false);
+      });
+  }, []);
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -10,11 +24,27 @@ function CountryInfo({ country }) {
       <h4>Languages</h4>
       <ul>
         {Object.entries(country.languages).map((language) => {
-          return <li>{language[1]}</li>;
+          return <li key={language[0]}>{language[1]}</li>;
         })}
       </ul>
 
-      <img src={country.flags.png} />
+      <img src={country.flags.png} alt="country flags" />
+
+      {!weatherLoading && (
+        <>
+          <h4>Weather in {country.capital[0]}</h4>
+          <div>
+            <b>Temperature:</b> {capitalWeather.main.temp} Celcius
+          </div>
+          <img
+            src={`http://openweathermap.org/img/wn/${capitalWeather.weather[0].icon}@2x.png`}
+            alt="weather icon"
+          />
+          <div>
+            <b>Wind:</b> {capitalWeather.wind.speed}
+          </div>
+        </>
+      )}
     </div>
   );
 }
